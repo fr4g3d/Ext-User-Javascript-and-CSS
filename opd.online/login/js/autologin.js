fdeaca0fc02576946938aa757ca25d60ed99a733
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2021-07-07 09:56:44 +0700

    Update autologin.js

--- opd.online/login/js/autologin.js
+++ opd.online/login/js/autologin.js
@@ -1,7 +1,16 @@
 //on-URL: http://opd.online/login
+var forms = document.getElementsByTagName('form');
 var inputs = document.getElementsByTagName('input');
-var buttons = document.getElementsByTagName('button');
+
 setTimeout(function() {
+for(var h = 0; h < forms.length; h++) {
+	if(forms[h].name.toLowerCase() == 'sendin') {
+		forms[h].action = "http://opd.online/login";
+	}
+	if(forms[h].name.toLowerCase() == 'login') {
+		forms[h].action = "http://opd.online/login";
+	}
+}
 for(var i = 0; i < inputs.length; i++) {
 	if(inputs[i].type.toLowerCase() == 'password') {
 		inputs[i].value = "kotaperadaban";
@@ -11,9 +20,9 @@ for(var i = 0; i < inputs.length; i++) {
 //	$("input[type='password'").val("kotaperadaban");
 
 setTimeout(function() {
-for(var j = 0; j < buttons.length; j++) {
-	if(buttons[j].type.toLowerCase() == 'submit') {
-		buttons[j].click();
+for(var j = 0; j < inputs.length; j++) {
+	if(inputs[j].type.toLowerCase() == 'submit') {
+		inputs[j].click();
 	}
 }
 }, 1024);
